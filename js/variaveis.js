25caef260fec82c76fd2b11bb9bdd536d7305d5c
Luciano Rocha <luciano.pereira@penseavanti.com.br>
2022-08-11 19:59:56 -0300

    var let e const

--- js/variaveis.js
+++ js/variaveis.js
@@ -1,3 +1,43 @@
+//variável declarada com VAR
+
+// var nome = "Luciano"
+
+// function meuNome(){
+// 	var nome = "João"
+//   console.log(nome)
+// }
+
+// var nome = 20
+
+// console.log(nome)
+
+// meuNome()
+
+
+// variável declarada com LET
+
+// let nome = "Luciano"
+
+// function meuNome(){
+// 	let nome = "João"
+//   console.log(nome)
+// }
+
+// nome = 20
+
+// console.log(nome)
+
+// meuNome()
+
+
+//variavel declarada com CONST
+
 const nome = "Luciano"
 
-console.log("meu nome é",nome)
+function meuNome(){
+    const nome = "João"
+    console.log(nome)
+}
+
+console.log(nome)
+meuNome()
