3cea5edbbdde7dae5f0f60ba60f1e63851bfcacf
Luciano Rocha <luciano.pereira@penseavanti.com.br>
2022-08-25 20:29:31 -0300

    loops

---
+++ js/loops.js
@@ -0,0 +1,17 @@
+const frutas = ["banana","maça","pera"]
+
+//console.log(frutas.length)
+
+//console.log(frutas[1])
+
+/*  for(var i=0; i<frutas.length; i++){
+  console.log(frutas[i])
+ } */
+ 
+/*  for(var i of frutas){
+   console.log(i)
+ } */
+ 
+ /* frutas.forEach((item)=>console.log(item)) */
+ 
+ frutas.map(fruta => console.log(fruta))
